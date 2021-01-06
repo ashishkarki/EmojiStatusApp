eaaebd73f447296b8328f8934b438e8da757f1b0
Ashish Karki <4175758+ashishkarki@users.noreply.github.com>
2021-01-06 21:21:06 +0545

    Init commit: with a basic login screen with a google sign-in button

---
+++ app/src/main/java/com/karkia/emojistatusapp/LoginActivity.kt
@@ -0,0 +1,23 @@
+package com.karkia.emojistatusapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        // Configure Google Sign In
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        val client = GoogleSignIn.getClient(this, gso)
+        btnSignIn
+    }
+}
