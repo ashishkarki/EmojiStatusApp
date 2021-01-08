2bc76cd9cf4acb4c71c402a37ef366312df63044
Ashish Karki <4175758+ashishkarki@users.noreply.github.com>
2021-01-08 23:43:39 +0545

    Currently has MainActivity with logout and LoginActivity with login capability

--- app/src/main/java/com/karkia/emojistatusapp/MainActivity.kt
+++ app/src/main/java/com/karkia/emojistatusapp/MainActivity.kt
@@ -1,11 +1,46 @@
 package com.karkia.emojistatusapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+    private companion object {
+        private const val TAG = "MainActivity"
+    }
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        auth = Firebase.auth
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.miLogout) {
+            Log.i(TAG, "Logout")
+            // simply logout the user
+            auth.signOut()
+            // navigate back to login screen
+            val logoutIntent = Intent(this, LoginActivity::class.java)
+            // clear the whole back-stack
+            logoutIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(logoutIntent)
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }
